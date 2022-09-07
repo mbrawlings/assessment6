31a346317a6a3d9d77e005946b9c23838197d5ae
Matthew Rawlings <mbrawlings11@gmail.com>
2022-09-07 12:18:43 -0600

    css to styles

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.js'))
 })
 
-app.get('/css', (req, res) => {
+app.get('/styles', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.css'))
 })
 
